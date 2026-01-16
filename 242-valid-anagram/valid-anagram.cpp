20846698e2dc355f2f38db25e0ed3fd98e1ddccb
Sridda Joka <sridda@gmail.com>
2026-01-16 19:28:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (91.09%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,32 +1,6 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        /* o(n) solution using hasmap:
-        if(s.length()==t.length()){
-            unordered_map<char,int>s_map;
-            for(int i=0;i<s.length();i++){
-                if (s_map.contains(s[i])){
-                    s_map[s[i]]++;
-                }else{
-                    s_map[s[i]]=1;
-                }
-            }
-            for(int i=0;i<t.length();i++){
-                if(s_map.contains(t[i])){
-                    if(s_map[t[i]]!=0) s_map[t[i]]--;
-                    else return false;
-                }else{
-                    return false;
-                }
-            }
-            return true;
-
-        }else{
-            return false;
-        }
-    }
-    */
-    //Most optimal solution:
         if(s.length()!=t.length()) return false;
         int arr[26]={0};
         for(char c: s){
