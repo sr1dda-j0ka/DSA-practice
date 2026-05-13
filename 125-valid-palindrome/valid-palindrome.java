c0f20aaff74c9ce24b65ebcc77e00b3cb8f55486
Sridda Joka <sridda@gmail.com>
2026-05-13 22:59:45 +0530

    Time: 2 ms (99.32%) | Memory: 44 MB (91.21%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,16 +1,17 @@
 class Solution {
+    //Idea is to use two pointers to compare from one end to other
     public boolean isPalindrome(String s) {
         int n=s.length();
         int left=0;
         int right=n-1;
         while(left<right){
-            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){ //check and move when non-alphanumeric character
                 left++;
             }
-            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){ //check and move for non-alphanumeric character
                 right--;
             }
-            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){//compare both ends 
                 return false;
             }
             left++;
