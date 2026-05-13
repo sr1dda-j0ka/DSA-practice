c968a25deda1cf77c71f019eb09eb047619f3633
Sridda Joka <sridda@gmail.com>
2026-05-13 23:05:24 +0530

    Time: 1 ms (21.73%) | Memory: 48.3 MB (37.07%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    //Straightforward array reversal using standard method
+    public void reverseString(char[] s) {
+        for(int i=0;i<s.length/2;i++){
+            char temp=s[i];
+            s[i]=s[s.length-i-1];
+            s[s.length-i-1]=temp;
+        }
+    }
+}
