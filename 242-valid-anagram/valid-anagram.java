04b15e5e90ea8ba3f544e2ee8cf6dad5e853574f
Sridda Joka <sridda@gmail.com>
2026-05-14 22:23:38 +0530

    Time: 3 ms (91.58%) | Memory: 44.3 MB (74.84%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,17 +1,19 @@
 class Solution {
+    //Approach is to create a frequency array that stores the frequencies of all 26 alphabets in a string
     public boolean isAnagram(String s, String t) {
-        int[] arr1=new int[26];
-        int[] arr2=new int[26];
+        int[] arr1=new int[26]; //frequency array for s
+        int[] arr2=new int[26];//frequency array for t
         for(int i=0;i<s.length();i++){
-            int n=s.charAt(i)-'a';
+            int n=s.charAt(i)-'a'; //we update the frequency array
             arr1[n]++;
         }
         for(int i=0;i<t.length();i++){
-            int n=t.charAt(i)-'a';
+            int n=t.charAt(i)-'a'; //we update the frequency array
             arr2[n]++;
         }
-        return Arrays.equals(arr1,arr2);
+        return Arrays.equals(arr1,arr2); //compare the frequency array
 
 
     }
+    //O(n) time complexity
 }
