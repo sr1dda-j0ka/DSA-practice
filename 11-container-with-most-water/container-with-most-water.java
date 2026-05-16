68e28246301a8c3be87984c6d8a274ec4547e621
Sridda Joka <sridda@gmail.com>
2026-05-16 23:26:57 +0530

    Time: 5 ms (82.64%) | Memory: 77.3 MB (47.68%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,16 +1,17 @@
 class Solution {
-    public int maxArea(int[] height) {
+    //Important - Use two pointer and move the one pointing to smaller value
+    public int maxArea(int[] height) { //Sliding Window Approach
         int left=0,right=height.length-1,area=0;
         while(left<right){
-            int h=Math.min(height[left],height[right]);
-            int w=right-left;
-            area=Math.max(area,h*w);
-            if(height[left]<height[right]){
-                left++;
+            int h=Math.min(height[left],height[right]); //height is the height of the smallest wall
+            int w=right-left; //width is the difference between the walls
+            area=Math.max(area,h*w); //keep measuring and storing maximum area
+            if(height[left]<height[right]){ 
+                left++; //increment if left points to smaller value
             }else{
-                right--;
+                right--; //decrement if right points to smaller value
             }
         }
-        return area;
+        return area; //return the final answer
     }
 }
