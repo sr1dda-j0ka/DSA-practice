58b16889cfde1e91b3300a320d6caa0ff271a0d4
Sridda Joka <sridda@gmail.com>
2026-05-18 23:27:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (15.49%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,9 +1,11 @@
 class Solution {
+    //Optimal Solution: Transpose the matrix and reverse each row 
+    //Time: O(n^2) Space: O(1)
     public void rotate(int[][] matrix) {
-        int n=matrix.length;
-        int m=matrix[0].length;
+        int n=matrix.length; //store no. of rows
+        int m=matrix[0].length; //store no. of columns
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){ //this loop swaps the upper triangular matrix with the lower to get transpose
             for(int j=i+1;j<n;j++){
                 int temp=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
@@ -11,9 +13,9 @@ class Solution {
             }
         }
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){ //this loop iterates through each row
             int j=0,k=m-1;
-            while(j<k){
+            while(j<k){ //reverses array using two pointers
                 int temp=matrix[i][k];
                 matrix[i][k]=matrix[i][j];
                 matrix[i][j]=temp;
