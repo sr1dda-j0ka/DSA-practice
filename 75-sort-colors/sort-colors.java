42cc71ddae7bf3c78c005de6a7ff79c39bbe861a
Sridda Joka <sridda@gmail.com>
2026-05-20 14:48:14 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (87.85%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,18 +1,23 @@
 class Solution {
+    //Brute Force: Normal Sort with O(nlogn) complexity
+    //Better: Counting Sort with O(n)
+    //Best: Dutch National Flag Alg: One Pass with O(n)
     public void sortColors(int[] nums) {
+        //declare three pointers 
         int low=0,mid=0,high=nums.length-1;
+        //mid traverses through array
         while(mid<=high){
-            if(nums[mid]==0){
-                int temp=nums[low];
+            if(nums[mid]==0){ //mid points to zero, swap with low element and increment low pointer
+                int temp=nums[low]; 
                 nums[low]=nums[mid];
                 nums[mid]=temp;
                 low++;
                 mid++;
             }
-            else if(nums[mid]==1){
+            else if(nums[mid]==1){//mid points to 1, let it be and move forward
                 mid++;
             }
-            else{
+            else{//mid points to 2, swap with high and decrement high
                 int temp=nums[high];
                 nums[high]=nums[mid];
                 nums[mid]=temp;
