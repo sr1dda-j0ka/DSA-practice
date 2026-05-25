4206a5bbd02b67a866a5840bf83e81c255d48457
Sridda Joka <sridda@gmail.com>
2026-05-25 23:33:14 +0530

    Time: 1 ms (99.95%) | Memory: 77 MB (81.06%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,11 +1,13 @@
 class Solution {
+    //Kadane's Algorithm: Time: O(n) Space: O(1)
     public int maxSubArray(int[] nums) {
+        //maxSum stores minimum value, sum stores sum so far
         int maxSum=Integer.MIN_VALUE,sum=0;
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
-            if(sum>maxSum) maxSum=sum;
+            if(sum>maxSum) maxSum=sum; //update max sum at every step
 
-            if(sum<0) sum=0;
+            if(sum<0) sum=0; //if sum is less than zero, bring sum back to zero
         }
         return maxSum;
     }
