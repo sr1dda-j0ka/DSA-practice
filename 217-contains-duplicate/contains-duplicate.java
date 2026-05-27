e8f6685b76f61f49f5b5956252dce83f9bc0afd6
Sridda Joka <sridda@gmail.com>
2026-05-27 22:48:02 +0530

    Time: 14 ms (88.79%) | Memory: 93.5 MB (50.17%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,12 +1,15 @@
 class Solution {
+    //Time Complexity: O(n) 
     public boolean containsDuplicate(int[] nums) {
+        //Create a hashset
         HashSet set=new HashSet<>();
+        //iterate and add elements to the hashset
         for(int i=0;i<nums.length;i++){
             if(set.contains(nums[i])){
-                return true;
+                return true; //if element already exists return true
             }
             set.add(nums[i]);
         }
-        return false;
+        return false; //return false if repeating element not flagged
     }
 }
