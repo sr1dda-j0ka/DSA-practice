b0bcf745e655aed7e6c223de8f96b500ab6ccf32
Sridda Joka <sridda@gmail.com>
2026-05-27 23:09:11 +0530

    Time: 4 ms (82.48%) | Memory: 44.2 MB (82.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,13 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        int[] freq=new int[26];
+        //O(n) approach
+        if(s.length()!=t.length()) return false; //return false if length don't match
+        int[] freq=new int[26]; //initialise frequency array
+        //increment characters of s
         for(int i=0;i<s.length();i++){
             freq[s.charAt(i)-'a']++;
         }
+        //decrement characters of t
         for(int j=0;j<t.length();j++){
             freq[t.charAt(j)-'a']--;
         }
+        //if both are anagrams, all frequencies should be zero
         for(int i=0;i<freq.length;i++){
             if(freq[i]!=0){
                 return false;
