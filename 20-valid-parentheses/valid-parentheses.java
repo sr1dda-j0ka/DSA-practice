5d68c2f904c9e03d8586888f83b07adbe2d2ccf1
Sridda Joka <sridda@gmail.com>
2026-05-30 23:47:42 +0530

    Time: 3 ms (88.08%) | Memory: 43.4 MB (40.11%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean isValid(String s) {
-        Deque<Character> stack=new ArrayDeque<>();
+        Deque<Character> stack=new ArrayDeque<>(); //initialize a stack
         for(char c: s.toCharArray()){
             if(c=='('||c=='['||c=='{'){
-                stack.push(c);
+                stack.push(c); //push the element into stack if it is a left bracket
             }else{
-                if(stack.isEmpty()) return false;
+                if(stack.isEmpty()) return false; 
                 
-                char top=stack.pop();
+                char top=stack.pop(); //if top element is not corresponding to char return false
                 if(top=='(' && c!=')') return false;
                 if(top=='[' && c!=']') return false;
                 if(top=='{' && c!='}') return false;
             }
         }
-        return stack.isEmpty();
+        return stack.isEmpty(); //true if empty stack
     }
 }
