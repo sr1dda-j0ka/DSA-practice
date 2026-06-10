bf354717c98b41adbbbd43cfb74b0fbf40076588
Sridda Joka <sridda@gmail.com>
2026-06-10 23:35:55 +0530

    Time: 2 ms (40.97%) | Memory: 94.5 MB (31.46%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,13 +1,11 @@
 class Solution {
-    //Similar to maximum sum subarray problem,Kadane's Algorithm O(n) time-complexity
     public int maxProfit(int[] prices) {
-        int min=prices[0];
-        int maxProfit=0;
+        int buyVal=prices[0],maxProfit=0;
         for(int i=1;i<prices.length;i++){
-            if(prices[i]<min){
-                min=prices[i]; //store the minimum element
-            }else{
-                maxProfit=Math.max(maxProfit,prices[i]-min); //check if maximum profit occurs at each element
+            int sellVal=prices[i];
+            maxProfit=Math.max(sellVal-buyVal,maxProfit);
+            if(prices[i]<buyVal){
+                buyVal=prices[i];
             }
         }
         return maxProfit;
