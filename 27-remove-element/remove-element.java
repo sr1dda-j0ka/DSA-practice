2559eae55a40362e4d39beac02c0cdb1d25e7e46
Sridda Joka <sridda@gmail.com>
2026-06-19 18:55:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (95.15%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -2,7 +2,7 @@ class Solution {
     public int removeElement(int[] nums, int val) {
         //Time complexity: O(n)
         //Traverse and push the desired element first
-        //Use another tracker variable
+        //Use another tracker variable(count)
         int count=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==val){
