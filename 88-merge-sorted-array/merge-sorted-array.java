02a660080b8cb1c1182211f9f24b7a54ff3bbc3f
Sridda Joka <sridda@gmail.com>
2026-06-21 18:01:46 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (14.66%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -2,6 +2,7 @@ class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         //Two pointers each pointing to end of the both the array elements, the third at end of first array zeroes
         //compare and put the larger number at the third pointer and decrement both
+        //Copy remaining elements from second array into first array
         int pointer1=m-1,pointer2=n-1,insertPointer=m+n-1;
         while(pointer1>=0&&pointer2>=0){
             if(nums1[pointer1]>nums2[pointer2]){
