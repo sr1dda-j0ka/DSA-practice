6ec6cb9e12ffe3d316982d35075883fda8791645
Sridda Joka <sridda@gmail.com>
2026-06-21 17:59:41 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (97.23%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
         int pointer1=m-1,pointer2=n-1,insertPointer=m+n-1;
         while(pointer1>=0&&pointer2>=0){
             if(nums1[pointer1]>nums2[pointer2]){
