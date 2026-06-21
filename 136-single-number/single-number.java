1b869ac898eb175d6277f7a4df7e22be482ed1c8
Sridda Joka <sridda@gmail.com>
2026-06-21 17:59:05 +0530

    Time: 1 ms (99.97%) | Memory: 46.7 MB (91.36%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,4 +1,5 @@
 class Solution {
+    //XOR property: duplicates cancel each other out to 0 and 0^x=x
     public int singleNumber(int[] nums) {
         int ans=0;
         for(int i=0;i<nums.length;i++){
