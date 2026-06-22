be4130523f524247aaf8a038825ed80a8f2d82a5
Sridda Joka <sridda@gmail.com>
2026-06-22 14:32:02 +0530

    Time: 1 ms (99.93%) | Memory: 94.4 MB (69.28%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,5 +1,6 @@
 class Solution {
     //Time Complexity-O(n) Space-O(1)
+    //Traverse each and record minimum element and maximum profit
     public int maxProfit(int[] prices) {
         int min=Integer.MAX_VALUE,profit=0;
         for(int i=0;i<prices.length;i++){
