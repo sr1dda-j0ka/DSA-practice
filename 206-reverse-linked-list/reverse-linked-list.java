a671aed469a1b0ea90b626e2496c6798ef1a69fc
Sridda Joka <sridda@gmail.com>
2026-06-24 00:06:21 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (49.23%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -9,18 +9,19 @@
  * }
  */
 class Solution {
-    //Time Complexity: O(n) Space Complexity: O(1)
-    //Approach: traverse the list and save the next element before changing next pointer to previous
-    //continue iteration
     public ListNode reverseList(ListNode head) {
-        ListNode curr=head; //current element
-        ListNode prev=null; //previous element
-        while(curr!=null){
-            ListNode next=curr.next; //next element is stored
-            curr.next=prev; //replace current with previous element
-            prev=curr;
-            curr=next; //move to next element 
+        if(head==null){
+            return head;
         }
-        return prev; //return head of reversed list
+        ListNode prev=null;
+        ListNode curr=head;
+        ListNode next=curr.next;
+        while(curr!=null){
+            ListNode temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
     }
 }
