bb4b050c4797eb74f8cbb12406901fd81880bc82
Sridda Joka <sridda@gmail.com>
2026-06-25 12:00:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (56.39%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,23 +10,32 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy=new ListNode(-1); //create a dummy node
-        ListNode tail=dummy; //tail will iterate 
-        while(list1!=null && list2!=null){
-            if(list1.val<=list2.val){ //find the smaller element
-                tail.next=list1; //attach it to the tail
-                list1=list1.next; //move pointer forward
+        ListNode dummy=new ListNode(0,null);
+        ListNode pointer=dummy;
+        ListNode temp1=list1;
+        ListNode temp2=list2;
+        while(temp1!=null&&temp2!=null){
+            if(temp1.val<=temp2.val){
+                pointer.next=temp1;
+                pointer=pointer.next;
+                temp1=temp1.next;
             }else{
-                tail.next=list2; 
-                list2=list2.next;
+                pointer.next=temp2;
+                pointer=pointer.next;
+                temp2=temp2.next;
             }
-            tail=tail.next; //move tail forward
         }
+        while(temp1!=null){
+            pointer.next=temp1;
+            pointer=pointer.next;
+            temp1=temp1.next;
+        }
+        while(temp2!=null){
+            pointer.next=temp2;
+            pointer=pointer.next;
+            temp2=temp2.next;
+        }
+        return dummy.next;
 
-        if(list1!=null) tail.next=list1; //attach remaining elements
-        else tail.next=list2;
-
-        return dummy.next; //return the head
-        
     }
 }
