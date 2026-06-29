d710c9b95da8ab8529a66931f57088a774056926
Sridda Joka <sridda@gmail.com>
2026-06-29 23:42:08 +0530

    Time: 5 ms (5.49%) | Memory: 43.3 MB (50.64%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String result="";
+        Arrays.sort(strs);
+        String first=strs[0],last=strs[strs.length-1];
+        for(int i=0;i<Math.min(first.length(),last.length());i++){
+            if(first.charAt(i)!=last.charAt(i)) return result;
+            result+=first.charAt(i);
+        }
+        return result;
+    }
+}
