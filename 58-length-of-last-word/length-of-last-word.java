4a7dc8e4f690a8a455d1996d3664de7f0f53b5ab
Sridda Joka <sridda@gmail.com>
2026-06-29 12:20:25 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (69.48%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,6 +1,13 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] stringArray=s.split(" ");
-        return stringArray[stringArray.length-1].length();
+        int count=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)==' '){
+                if(count>0) return count;
+            }else{
+                count++;
+            }
+        }
+        return count;
     }
 }
