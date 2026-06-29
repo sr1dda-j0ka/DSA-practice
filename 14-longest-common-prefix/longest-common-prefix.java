b7bd948cfd9643520e74e4d95d49c353809fa887
Sridda Joka <sridda@gmail.com>
2026-06-29 23:44:52 +0530

    Time: 1 ms (62.63%) | Memory: 43.5 MB (17.33%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -3,13 +3,13 @@ class Solution {
         //Sort the array in lexicographical order
         //then compare the first and last string till one of them ends
         //keep track of result and return it in the end
-        String result="";
+        StringBuilder sb=new StringBuilder();
         Arrays.sort(strs);
         String first=strs[0],last=strs[strs.length-1];
         for(int i=0;i<Math.min(first.length(),last.length());i++){
-            if(first.charAt(i)!=last.charAt(i)) return result;
-            result+=first.charAt(i);
+            if(first.charAt(i)!=last.charAt(i)) return sb.toString();
+            sb.append(first.charAt(i));
         }
-        return result;
+        return sb.toString();
     }
 }
