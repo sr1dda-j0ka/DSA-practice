d5d974b7c90f721667fb9f9280f174e6f2b82779
Sridda Joka <sridda@gmail.com>
2026-07-02 16:43:13 +0530

    Time: 1 ms (67.14%) | Memory: 42.8 MB (57.60%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open=0,close=0;
+         for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch==')'){
+                if(open==0) close++;
+                else open--;
+            }
+            if(ch=='('){
+                open++;
+            }
+         }
+         return open+close;
+    }
+}
